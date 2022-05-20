6be235bebdfbb78db2c7b3b8be2412d1640c840e
seairs <sea.airs77@gmail.com>
2022-05-19 23:46:42 -0400

    added new/create/show for bookings, index for flights

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
+  root 'flights#index'
+  resources :bookings, only: [:new, :create, :show]
+  resources :flights, only: [:index]
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
