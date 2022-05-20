e361f6db7b2a3dfdd5818f6ef094c832a229b823
seairs <sea.airs77@gmail.com>
2022-05-19 23:47:11 -0400

    created schema and associations

---
+++ app/models/passenger.rb
@@ -0,0 +1,20 @@
+# == Schema Information
+#
+# Table name: users
+#
+#  id                   :integer          not null, primary key
+#  name                 :string
+#  email                :string
+#  created_at           :datetime         not null
+#  updated_at           :datetime         not null
+#
+class Passenger < ApplicationRecord
+  # The user belongs to a booking
+  belongs_to :booking
+
+  # Validations
+  # Name must be present, must be less than 20 characters
+  # Email must be present, must confirm to email format, must be less than 20 characters
+  validates :name, presence: true, length: { maximum: 20 }
+  validates :email, presence: true, format: { with: URI::MailTo::EMAIL_REGEXP }, length: { maximum: 20 }
+end
