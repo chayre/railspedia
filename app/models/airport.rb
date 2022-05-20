c49b9c096c62c39e78ca6f80abbaa0702421243e
seairs <sea.airs77@gmail.com>
2022-05-19 23:47:33 -0400

    created schema and associations

---
+++ app/models/airport.rb
@@ -0,0 +1,14 @@
+# == Schema Information
+#
+# Table name: bookings
+#
+#  id                   :integer          not null, primary key
+#  airpirt_id           :string
+#  created_at           :datetime         not null
+#  updated_at           :datetime         not null
+#
+class Airport < ApplicationRecord
+  # Each airport has many outgoing and inbound flights
+  has_many :departing_flights, :foreign_key => :departure_airport_id, class_name: "Flight"
+  has_many :arriving_flights, :foreign_key => :arrival_airport_id, class_name: "Flight"
+end
