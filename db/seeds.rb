bb0881b2f33aa1015ced6428282dbfe0789fdb45
seairs <sea.airs77@gmail.com>
2022-05-20 21:27:13 -0400

    forgot to convert first numberset to array

--- db/seeds.rb
+++ db/seeds.rb
@@ -18,7 +18,7 @@ airports = Airport.create([
 # We'll map the 14 airports to a number (1 - 14)
 # Product will do all the permutations, we'll convert that to an array of combos
 # However, this contains duplicate combos (for example, [7, 7]) so we remove those with reject
-flights_constructor = ((1..14).product((1..14)).to_a).reject { |combo| combo[0] == combo[1] } 
+flights_constructor = (1..14).to_a.product((1..14)).to_a).reject { |combo| combo[0] == combo[1] } 
 # flights_constructor contains every airport combo ([LAX, CLT], [PHX, MCO] ...); iterate through each combo
 flights_constructor.each do |flight_combo|
   # Make 25 to 40 random flights for each airport combo
