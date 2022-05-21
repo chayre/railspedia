d168db97b81a36919189558f09949fc10d626b7a
seairs <sea.airs77@gmail.com>
2022-05-20 22:23:39 -0400

    created search method to return flights that match user query

--- app/models/flight.rb
+++ app/models/flight.rb
@@ -11,10 +11,22 @@
 #  updated_at              :datetime         not null
 #
 class Flight < ApplicationRecord
-    # Each flight can have many bookings
-    has_many :bookings
-    # The flight's departure airport belongs to the airport
-    belongs_to :departure_airport, class_name: "Airport"
-    # The flight's departure airport belongs to the airport
-    belongs_to :arrival_airport, class_name: "Airport"
+  # Each flight can have many bookings
+  has_many :bookings
+  # The flight's departure airport belongs to the airport
+  belongs_to :departure_airport, class_name: "Airport"
+  # The flight's departure airport belongs to the airport
+  belongs_to :arrival_airport, class_name: "Airport"
+
+  # Method to select and return all flights that match user query (submitted via params)
+  def self.search(params)
+    # Convert user submitted time to datetime of form month/day/year
+    date = DateTime.strptime(params[:date], "%m/%d/%Y")
+    # Select Flights from database where the departure_airport, arrival_airport, and departure_time match the user's query
+    Flight.where(
+      departure_airport_id: params[:departure_airport_id],
+      arrival_airport_id: params[:arrival_airport_id],
+      departure_time: date
+      )
+    end
 end
