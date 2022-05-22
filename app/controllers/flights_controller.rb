606b54a1b8539fd77aeb83934e3ca30355948a6c
seairs <sea.airs77@gmail.com>
2022-05-21 22:07:45 -0400

    changed flash message to alert, deleted unused methods

--- app/controllers/flights_controller.rb
+++ app/controllers/flights_controller.rb
@@ -1,38 +1,22 @@
 class FlightsController < ApplicationController
-  before_action :set_flight, only: %i[ show edit update destroy ]
-
+ 
   # GET /flights or /flights.json
   def index
     # When index is called, return @dates instance variable which is the dates of all flights ordered (ascending) by time and mapped to month, date, year format
     # .uniq, otherwise we'll get a bunch of repeat dates
-    @dates = Flight.all.order(:departure_time).map { |flight| flight.departure_time.strftime("%m/%d/%Y") }.uniq
+    @dates = Flight.all.order(:departure_time).map { |flight| (flight.departure_time.tomorrow).strftime("%m/%d/%Y") }.uniq
     @airports = Airport.all.map{ |airport| [airport.airport_id, airport.id] }
-    
-    
+
+    # If the user hits search but both airports are the same, send a flash.now alert message
     if params[:commit] == "Search"
       if params[:departure_airport_id] == params[:arrival_airport_id]
-        flash.now[:error] = "Departure and arrival airports can't be the same."
+        flash.now[:alert] = "Departure and arrival airports can't be the same."
       end
     end
-
-
+    
+    # After the user submits params with a date, return the flights corresponding to that date (#search method in flight model)
     if params[:date]
       @flights = Flight.search(params)
     end
   end
-
-  # GET /flights/1 or /flights/1.json
-  #def show
- # end
-
-  #private
-    # Use callbacks to share common setup or constraints between actions.
-    #def set_flight
-    #  @flight = Flight.find(params[:id])
-    #end
-
-    # Only allow a list of trusted parameters through.
-   # def flight_params
-   #   params.permit(:flight).permit(:departure_airport_id, :arrival_airport_id, :departure_time, :flight_duration, :date, :commit)
-  #  end
 end
