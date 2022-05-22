380510e8872e8fa1e685aa8d6d83869a3a0c4cb0
seairs <sea.airs77@gmail.com>
2022-05-21 21:55:00 -0400

    deleted extra comments

--- config/routes.rb
+++ config/routes.rb
@@ -2,8 +2,4 @@ Rails.application.routes.draw do
   root 'flights#index'
   resources :bookings, only: [:new, :create, :show]
   resources :flights, only: [:index]
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
-  # Defines the root path route ("/")
-  # root "articles#index"
 end
