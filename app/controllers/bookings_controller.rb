4fde118faf7c873d612234664e0447f612cc3773
seairs <sea.airs77@gmail.com>
2022-05-21 22:07:19 -0400

    added show methods to load booking/flight/passenger objects from db

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -1,58 +1,45 @@
 class BookingsController < ApplicationController
-  before_action :set_booking, only: %i[ show edit update destroy ]
-
+  
   # GET /bookings or /bookings.json
   def index
     @bookings = Booking.all
   end
 
-  # GET /bookings/1 or /bookings/1.json
-  def show
-  end
-
   # GET /bookings/new
   def new
+    # Find values from by querying active record with submitted params
     @flight = Flight.find_by(id: params[:flight_id])
     @booking = @flight.bookings.build
     @passenger_number = params[:passengers].to_i
     @passenger_number.times { @booking.passengers.build }
   end
 
-  # POST /bookings or /bookings.json
   def create
-    @booking = Booking.new(booking_params)
-
-    respond_to do |format|
-      if @booking.save
-        format.html { redirect_to booking_url(@booking), notice: "Booking was successfully created." }
-        format.json { render :show, status: :created, location: @booking }
-      else
-        format.html { render :new, status: :unprocessable_entity }
-        format.json { render json: @booking.errors, status: :unprocessable_entity }
-      end
+    @booking = Booking.create!(booking_params)
+    # This if/else calls .save; if successful, it will redirect to the show page and flash a notice. Otherwise it redirects to new page again and flashes alert.
+    if @booking.save
+      flash[:notice] = "Booking submitted!"
+      redirect_to @booking
+    else
+      flash[:alert] = "Submission error"
+      redirect_to @booking.new
     end
   end
 
-  # PATCH/PUT /bookings/1 or /bookings/1.json
-  def update
-    respond_to do |format|
-      if @booking.update(booking_params)
-        format.html { redirect_to booking_url(@booking), notice: "Booking was successfully updated." }
-        format.json { render :show, status: :ok, location: @booking }
-      else
-        format.html { render :edit, status: :unprocessable_entity }
-        format.json { render json: @booking.errors, status: :unprocessable_entity }
-      end
-    end
+  def show
+    # Load the booking we just created by querying the database with the submitted id
+    @booking = Booking.find(params[:id])
+    # Load the flight associated with the booking by querying the database with the submitted id
+    @flight = Flight.find(@booking.flight_id)
+    # Load the submitted passengers by searching the passenger table for booking_ids that match the submitted id
+    @passenger = Passenger.where(booking_id: params[:id])
   end
 
-  # DELETE /bookings/1 or /bookings/1.json
-  def destroy
-    @booking.destroy
+  private
 
-    respond_to do |format|
-      format.html { redirect_to bookings_url, notice: "Booking was successfully destroyed." }
-      format.json { head :no_content }
-    end
+  def booking_params 
+    params.require(:booking).permit(:flight_id,
+                                    :passengers_attributes => [:name, :email])
   end
+
 end
