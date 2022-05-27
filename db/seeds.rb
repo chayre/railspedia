facd7271669e835fc1e9a0f03cc70a1fef3eae34
seairs <sea.airs77@gmail.com>
2022-05-26 23:37:05 -0400

    reduce size of database seed (too large for Heroku)

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,4 +1,4 @@
-# Seed our database with the 14 largest airports in the US
+# Seed our database with the 10 airports in the US
 airports = Airport.create([
 { airport_id: 'IAD' },
 { airport_id: 'ATL' },
@@ -9,21 +9,18 @@ airports = Airport.create([
 { airport_id: 'CLT' },
 { airport_id: 'LAS' },
 { airport_id: 'PHX' },
-{ airport_id: 'MCO' },
-{ airport_id: 'SEA' },
-{ airport_id: 'MIA' },
-{ airport_id: 'IAH' },
-{ airport_id: 'JFK' }])
+{ airport_id: 'JFK' }
+])
 
-# We'll map the 14 airports to a number (1 - 14)
+# We'll map the 10 airports to a number (1 - 10)
 # Product will do all the permutations, we'll convert that to an array of combos
 # However, this contains duplicate combos (for example, [7, 7]) so we remove those with reject
-flights_constructor = (1..14).to_a.product((1..14).to_a).reject { |combo| combo[0] == combo[1] } 
+flights_constructor = (1..10).to_a.product((1..10).to_a).reject { |combo| combo[0] == combo[1] }
 # flights_constructor contains every airport combo ([LAX, CLT], [PHX, MCO] ...); iterate through each combo
 flights_constructor.each do |flight_combo|
-  # Make 120 to 150 random flights for each airport combo
-  rand(120..150).times do 
-    # Flight duration is randomized; departure time also randomized to two years from now and converted to datetime 
+  # Make 70 to 80 random flights for each airport combo
+  rand(70..80).times do
+    # Flight duration is randomized; departure time also randomized to one month from now and converted to datetime 
     Flight.create(
       departure_airport_id: flight_combo[0],
       arrival_airport_id: flight_combo[1],
